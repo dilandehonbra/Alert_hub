2a79b62d37e5c9b5be7a86ed351211ae4582c0dd
Dilan Santos de Oliveira <dilan.severino.oliveira@gmail.com>
2025-06-22 00:19:24 +0000

    sobe comitasso script pra dar commit

--- cmitasso.sh
+++ cmitasso.sh
@@ -2,7 +2,6 @@
 files=$(ls $PWD)
 
 desc=$1
-cmdlineOptions=$2
 for i in $(echo "$files") ; do git add $PWD/$i ; done
 
-git commit -m "${desc}" ${cmdLineOptions} && git push origin main
+git commit -m "${desc}" && git push origin main
